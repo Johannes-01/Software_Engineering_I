df3ee6c4551fddad79f9fbbf0bf11ca5f765b7f1
Simon Krempin <67335158+simonkrempin@users.noreply.github.com>
2024-11-23 14:13:37 +0100

    feat(#41): installed vitest and added a first mock tset

---
+++ project_micheal_angelo/src/app/api/category/route.test.ts
@@ -0,0 +1,7 @@
+import { describe, expect, it, } from "vitest";
+
+describe("This is a sample test", () => {
+    it("should pass the pipeline", () => {
+        expect(2 + 1).toBe(3);
+    });
+});
