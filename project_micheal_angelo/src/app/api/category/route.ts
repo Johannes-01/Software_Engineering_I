e46d8aec1285139bc284ee9004aaf2fd04c54b34
Simon Krempin <67335158+simonkrempin@users.noreply.github.com>
2024-11-30 15:53:38 +0100

    refactoring(#14): get and post now also use the precondition handler

--- project_micheal_angelo/src/app/api/category/route.ts
+++ project_micheal_angelo/src/app/api/category/route.ts
@@ -1,52 +1,60 @@
-import { handlerWithPreconditions, requireAdmin } from "@utils/custom-middleware";
-import { badRequestError, conflictError, internalServerError } from "@utils/server-errors";
-import { createSupabaseClient } from "@utils/supabase-helper";
-import { NextRequest, NextResponse } from "next/server";
+import {
+    handlerWithPreconditions,
+    MiddlewareContext,
+    requireAdmin,
+    requireUnique,
+    validateBody,
+} from "@utils/custom-middleware";
+import { internalServerError } from "@utils/server-errors";
+import { NextResponse } from "next/server";
+import z from "zod";
 
-export const GET = handlerWithPreconditions(
+interface GetContext extends MiddlewareContext {
+    supabaseClient: Exclude<MiddlewareContext["supabaseClient"], undefined>
+}
+
+export const GET = handlerWithPreconditions<GetContext>(
     [requireAdmin],
-    async ({ supabaseClient }, _1, _2) => {
-        supabaseClient ??= await createSupabaseClient()
-
+    async ({
+        supabaseClient,
+        route,
+    }) => {
         const {
             data,
             error,
-        } = await supabaseClient.from('category').select('*');
+        } = await supabaseClient.from('category').select('*')
 
         if (error) {
-            console.error("/api/category:GET -> ", error.message);
-            return internalServerError();
+            console.error(`${route} -> `, error.message)
+            return internalServerError()
         }
 
-        return NextResponse.json(data, { status: 200 });
-    }
+        return NextResponse.json(data, { status: 200 })
+    },
+    {
+        route: "/api/category:GET",
+    },
 )
 
-export const POST = handlerWithPreconditions(
-    [requireAdmin],
-    async ({ supabaseClient }, request: NextRequest, _) => {
-        supabaseClient ??= await createSupabaseClient()
+interface PostContext extends MiddlewareContext {
+    supabaseClient: Exclude<MiddlewareContext["supabaseClient"], undefined>
+    body: { name: string }
+}
 
-        const { name } = await request.json();
+const postSchema = z.object({
+    name: z.string().min(1),
+})
 
-        if (!name) {
-            return badRequestError("name is missing in request body");
-        }
-
-        const {
-            data,
-            error: selectError,
-        } = await supabaseClient.from("category").select("name").eq("name", name);
-
-        if (selectError) {
-            console.error("/api/category:POST -> ", selectError.message);
-            return internalServerError();
-        }
-
-        if (!data || data.length > 0) {
-            return conflictError();
-        }
-
+export const POST = handlerWithPreconditions<PostContext>(
+    [
+        requireAdmin,
+        validateBody(postSchema),
+        async (context) => requireUnique("category", "name", context.body!.name)(context),
+    ],
+    async ({
+        supabaseClient,
+        body: { name },
+    }) => {
         const { error: insertError } = await supabaseClient.from("category").insert({ name });
 
         if (insertError) {
