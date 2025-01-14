3842fb0e8e15e2d300bdf376a98a2ac80da3dad7
Simon Krempin <simon.krempin@gmail.com>
2025-01-14 19:53:03 +0100

    chore(#81): refactoring gallery page

---
+++ project_micheal_angelo/src/app/(dashboard)/gallery/_local/gallery-card.tsx
@@ -0,0 +1,82 @@
+import React, { useState } from 'react';
+import { Card, CardContent } from "@components/ui/card";
+import { Button } from "@components/ui/button";
+import { Edit2, Trash } from "lucide-react";
+import { Item } from "@type/item";
+import Link from "next/link";
+
+interface GalleryCardProps {
+    image: Item;
+    userId: string | undefined;
+    userIsAdmin?: boolean;
+}
+
+const GalleryCard: React.FC<GalleryCardProps> = ({
+    image,
+    userId,
+    userIsAdmin,
+}) => {
+    const [isHovered, setIsHovered] = useState<boolean>(false)
+
+    return (
+        <Card
+            className="relative"
+            key={image.id}
+            onMouseEnter={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)}
+        >
+            <CardContent className="p-4 flex flex-col justify-between h-full">
+                <div>
+                    {(isHovered && userIsAdmin) && (
+                        <div className="absolute top-2 right-2 flex gap-2">
+                            <Link href={`/gallery/edit/${image.id}`}>
+                                <Button
+                                    variant="secondary"
+                                >
+                                    <Edit2/>
+                                </Button>
+                            </Link>
+                            <Button
+                                className="bg-red-500 hover:bg-red-600"
+                                onClick={() => {
+                                    // Handle delete action
+                                    // e.g., deleteImage(image.id);
+                                }}
+                            >
+                                <Trash className="text-white"/>
+                            </Button>
+                        </div>
+                    )}
+
+                    <img
+                        src={image.image_path}
+                        alt={image.title}
+                        width={300}
+                        height={400}
+                        className="w-full h-48 object-cover mb-2 rounded"
+                    />
+
+                    <h2 className="text-xl font-semibold">{image.title}</h2>
+                    <p className="text-gray-600">{image.notice}</p>
+                </div>
+
+                <div className="flex justify-between items-center mt-2">
+                    <p className="text-lg font-semibold">${image.price.toFixed(2)}</p>
+                    {
+                        userId
+                            ? <Link href={`/gallery/configure/${image.id}?userId=${userId}`}>
+                                <Button>
+                                    Konfigurieren
+                                </Button>
+                            </Link>
+                            : <Button disabled={true}>
+                                Konfigurieren
+                            </Button>
+                    }
+                </div>
+            </CardContent>
+        </Card>
+    );
+};
+
+export default GalleryCard;
---
+++ project_micheal_angelo/src/app/(dashboard)/gallery/_local/search-bar.tsx
@@ -0,0 +1,145 @@
+import React, { useState } from 'react';
+import { Input } from "@components/ui/input";
+import { Button } from "@components/ui/button";
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from "@components/ui/select"
+import useSWR from "swr";
+import { Category } from "@type/category";
+import Link from "next/link";
+
+interface SearchBarProps {
+    userIsAdmin?: boolean
+    currentPage: number
+    setImageQuery: (query: string) => void
+    users: { id: string, email: string }[]
+    setSelectedUser: (user: { id: string, email: string } | undefined) => void
+}
+
+const fetcher = async (url: string) => {
+    const response = await fetch(url);
+    if (!response.ok) {
+        throw new Error(`An error occurred while fetching the data from ${url}`);
+    }
+    return response.json();
+};
+
+const SearchBar: React.FC<SearchBarProps> = ({
+    userIsAdmin,
+    currentPage,
+    setImageQuery,
+    users,
+    setSelectedUser,
+}) => {
+    const [searchTerm, setSearchTerm] = useState<string>("")
+    const [artist, setArtist] = useState<string>("")
+    const [selectedCategory, setSelectedCategory] = useState(0)
+
+    const { data: availableCategories } = useSWR<Category[]>("/api/category", fetcher);
+
+    const buildImageQuery = () => {
+        const params = new URLSearchParams();
+
+        params.append('p', currentPage.toString());
+
+        if (searchTerm) {
+            params.append('q', searchTerm);
+        }
+
+        if (artist) {
+            params.append('a', artist);
+        }
+
+        if (selectedCategory !== 0) {
+            params.append('c', selectedCategory.toString());
+        }
+
+        return `/api/image?${params.toString()}`;
+    };
+
+    return (
+        <div className={"w-full"}>
+            <div className="flex gap-4 mb-2 w-full">
+                <div className="w-4/5 flex gap-2">
+                    <Input
+                        placeholder="Search artworks..."
+                        value={searchTerm}
+                        onChange={(e) => setSearchTerm(e.target.value)}
+                        className="flex-1"
+                    />
+                    <Input
+                        placeholder="Search for artist..."
+                        value={artist}
+                        onChange={(e) => setArtist(e.target.value)}
+                        className="flex-1"
+                    />
+                </div>
+
+                <div className="w-3/20">
+                    <Select onValueChange={(value: string) => setSelectedCategory(Number(value))}>
+                        <SelectTrigger className="w-full">
+                            <SelectValue placeholder="Category"/>
+                        </SelectTrigger>
+                        <SelectContent>
+                            <SelectItem value="0">All Categories</SelectItem>
+                            {availableCategories?.map((category) => (
+                                <SelectItem
+                                    key={category.id}
+                                    value={category.id.toString()}
+                                >
+                                    {category.name.charAt(0).toUpperCase() + category.name.slice(1)}
+                                </SelectItem>
+                            ))}
+                        </SelectContent>
+                    </Select>
+                </div>
+
+                <div className="w-1/20">
+                    <Button
+                        className="w-full"
+                        onClick={() => setImageQuery(buildImageQuery())}
+                    >
+                        Search
+                    </Button>
+                </div>
+            </div>
+
+            {userIsAdmin && <div className="w-full flex justify-between">
+                <div className="w-3/20">
+                    {users && (
+                        <Select onValueChange={(value: string) => {
+                            setSelectedUser(users.find((user) => user.id ===  value))
+                        }}>
+                            <SelectTrigger className="w-full">
+                                <SelectValue placeholder="No User"/>
+                            </SelectTrigger>
+                            <SelectContent>
+                                <SelectItem value="no">No User</SelectItem>
+                                {users.map((user) => (
+                                    <SelectItem
+                                        key={user.id}
+                                        value={user.id}
+                                    >
+                                        {user.email}
+                                    </SelectItem>
+                                ))}
+                            </SelectContent>
+                        </Select>
+                    )}
+                </div>
+
+                <Link href={"category/"}>
+                    <Button>
+                        Add
+                    </Button>
+                </Link>
+            </div>}
+        </div>
+    );
+};
+
+export default SearchBar;
