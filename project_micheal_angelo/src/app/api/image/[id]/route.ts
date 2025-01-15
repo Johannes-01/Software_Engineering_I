852c1d30e88b01e7854b1e0a2a6413bdbaf405da
Johannes <johannes.haick@gmail.com>
2025-01-15 22:47:10 +0100

    feat(#10): create endpoint

--- project_micheal_angelo/src/app/api/image/[id]/route.ts
+++ project_micheal_angelo/src/app/api/image/[id]/route.ts
@@ -2,6 +2,60 @@
 
 import { NextResponse } from 'next/server';
 import { createSupabaseClient } from '@utils/supabase-helper'
+import { ItemRequest } from '@type/item';
+import { handlerWithPreconditions, MiddlewareContext, requireAdmin, requireExists, validateBody } from '@utils/custom-middleware';
+import { z } from 'zod';
+import { internalServerError } from '@utils/server-errors';
+
+interface PutContext extends MiddlewareContext {
+    supabaseClient: Exclude<MiddlewareContext["supabaseClient"], undefined>,
+    body: ItemRequest
+}
+
+type Slug = { params: { id: string } };
+
+const putBody = z.object({
+    category_id: z.number(),
+    title: z.string(),
+    artist: z.string(),
+    width: z.number(),
+    height: z.number(),
+    motive_width: z.number(),
+    motive_height: z.number(),
+    price: z.number(),
+    notice: z.string(),
+})
+
+const imageShouldExist = async (context: MiddlewareContext, _: unknown, { params }: Slug) => requireExists(
+    "image",
+    { id: params.id }
+)(context)
+
+export const PUT = handlerWithPreconditions<PutContext>(
+    [
+        requireAdmin,
+        validateBody(putBody),
+        imageShouldExist,
+    ],
+    async ({ supabaseClient, body, route }, _, { params }: Slug) => {
+        const { error } = await supabaseClient
+            .from("image")
+            .update({
+                ...(body.artist === undefined ? {} : { artist: body.artist }),
+            })
+            .eq("id", params.id)
+
+        if (error) {
+            console.error(`${route} -> ${error.message}`)
+            return internalServerError()
+        }
+
+        return new NextResponse("Updated", { status: 200 })
+    },
+    {
+        route: "/api/image/[id]:PUT",
+    },
+)
 
 export async function GET(request: Request,
     { params }: { params: { id: string } }
